a1cda5259750cf4a553c14e44a77ed41e737a0d8
Michael Zhang <michaelzhang@Claires-MacBook-Air.local>
2020-04-21 17:04:21 -0400

    added while

--- python/HW.py
+++ python/HW.py
@@ -1,3 +1,11 @@
-number = int(input("How many pounds do you weigh?\n"))
-weight = number * .454
-print(f"You are {weight} kilograms heavy.")
+while True:
+    text = input("How many pounds do you weigh?\n")
+    if text == "quit":
+        print("We are done. Bye!")
+        break
+    try:
+        number = float(text)
+        weight = number * .454
+        print(f"You are {weight} kilograms heavy.")
+    except ValueError:
+        print(f"What you typed, {text}, is not a number.  Please try again.")
