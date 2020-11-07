dc0875008f6c4da53c4ddc8647d7074effaa9fd7
Michael Zhang <michaelzhang@Claires-MacBook-Air.local>
2020-11-07 17:56:57 -0500

    chicken rabbit

--- python/stat.py
+++ python/stat.py
@@ -25,26 +25,30 @@ def mz_max(list):
             max = number
     return max
 
-# standard deviation
-# 1. get average
-# 2. get the diff squared
-# 3. average the diff squared
-# 4. square root
+def mz_sd(list):
+    avg = mz_average(list)
+    sumOfSquare = 0
+    for item in list:
+        square = pow(item - avg, 2)
+        sumOfSquare = sumOfSquare + square
+    avgOfSquare = sumOfSquare/mz_count(list)
+    result = pow(avgOfSquare, 1/2)
+    return result
 
-numbers = [5,4,3,2,1,0]
+numbers = [5, 4, 3, 2, 1, 0]
+moreNumbers = [2, 4, -1, 0.5]
 
-# count
 print(f"The count is {mz_count(numbers)}")
-print(f"The count is {mz_count([2, 4, -1, 0.5])}")
+print(f"The count is {mz_count(moreNumbers)}")
 
-# average
 print(f"The average is {mz_average(numbers)}")
-print(f"The average is {mz_average([2, 4, -1, 0.5])}")
+print(f"The average is {mz_average(moreNumbers)}")
 
-# min, find the smallest
 print(f"The smallest number is {mz_min(numbers)}")
-print(f"The smallest number is {mz_min([2, 4, -1, 0.5])}")
+print(f"The smallest number is {mz_min(moreNumbers)}")
 
-# max, find the biggest number
 print(f"The biggest number is {mz_max(numbers)}")
-print(f"The biggest number is {mz_max([2, 4, -1, 0.5])}")
+print(f"The biggest number is {mz_max(moreNumbers)}")
+
+print(f"The standard deviation of the numbers is {mz_sd(numbers)}")
+print(f"The standard deviation number is {mz_sd(moreNumbers)}")
