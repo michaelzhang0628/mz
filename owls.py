8491ef0cab757d97b0fc7b6ad6f3b29b2fb39ee2
michaelzhang0628 <63886073+michaelzhang0628@users.noreply.github.com>
2023-02-08 20:00:39 -0500

    Create owls.py

---
+++ owls.py
@@ -0,0 +1,8 @@
+def owl_count(text):
+    ow
+
+    return 0
+
+
+text = "I really like owls. Did you know that an owl's eyes are more than twice as big as the eyes of other birds of comparable weight? And that when an owl partially closes its eyes during the day, it is just blocking out light? Sometimes I wish I could be an owl."
+owl_count(text)
