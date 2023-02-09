0fa5cca064358854f621fe8745f390866337f884
michaelzhang0628 <63886073+michaelzhang0628@users.noreply.github.com>
2023-02-08 21:43:42 -0500

    Update owls.py

--- python/owls.py
+++ python/owls.py
@@ -1,13 +1,11 @@
 def owl_count(text):
-    period_list = text.split(".")
     count = 0
+    period_list = text.split(".")
     for item in period_list:
         question_mark_list = item.split("?")
-        for smaller_item in question_mark_list:
-            if "owl" in smaller_item.lower():
-                count += 1
+        for small_item in question_mark_list:
+            space_list = small_item.split(" ")
+            for tiny_item in space_list:
+                if "owl" in tiny_item.lower():
+                    count += 1
     return count
-
-
-words = "I really like owls. Did you know that an owl's eyes are more than twice as big as the eyes of other birds of comparable weight? And that when an owl partially closes its eyes during the day, it is just blocking out light? Sometimes I wish I could be an Owl."
-print("The number of times the word owls was said was " + str(owl_count(words)))
