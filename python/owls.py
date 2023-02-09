0e502b49046afc0bfb94db0a2f6d94b521919cf1
michaelzhang0628 <63886073+michaelzhang0628@users.noreply.github.com>
2023-02-08 21:32:07 -0500

    Update owls.py

--- python/owls.py
+++ python/owls.py
@@ -7,3 +7,7 @@ def owl_count(text):
             if "owl" in smaller_item.lower():
                 count += 1
     return count
+
+
+words = "I really like owls. Did you know that an owl's eyes are more than twice as big as the eyes of other birds of comparable weight? And that when an owl partially closes its eyes during the day, it is just blocking out light? Sometimes I wish I could be an Owl."
+print("The number of times the word owls was said was " + str(owl_count(words)))
