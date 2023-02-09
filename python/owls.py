ce2567326e6630be59b765c8352d20c234b7d6f5
michaelzhang0628 <63886073+michaelzhang0628@users.noreply.github.com>
2023-02-08 21:30:29 -0500

    Update owls.py

--- python/owls.py
+++ python/owls.py
@@ -1,8 +1,9 @@
 def owl_count(text):
-    ow
-
-    return 0
-
-
-text = "I really like owls. Did you know that an owl's eyes are more than twice as big as the eyes of other birds of comparable weight? And that when an owl partially closes its eyes during the day, it is just blocking out light? Sometimes I wish I could be an owl."
-owl_count(text)
+    period_list = text.split(".")
+    count = 0
+    for item in period_list:
+        question_mark_list = item.split("?")
+        for smaller_item in question_mark_list:
+            if "owl" in smaller_item.lower():
+                count += 1
+    return count
